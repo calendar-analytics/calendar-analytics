acbafb98dd3978760f1558f3c4f5f3e084c42f7d
MclPio <113801014+MclPio@users.noreply.github.com>
2024-08-12 17:31:41 -0400

    save work

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  provider :google_oauth2, ENV['google_client_id'], ENV['google_client_secret'],
     {
       scope: 'email, profile, https://www.googleapis.com/auth/calendar.events.readonly',
       prompt: 'select_account',
     }
 end
-OmniAuth.config.allowed_request_methods = %i[get]
+# OmniAuth.config.allowed_request_methods = %i[get]
