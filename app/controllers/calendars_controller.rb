16ea8a945b8ac7c21b3b1f17abb226d75386982d
MclPio <113801014+MclPio@users.noreply.github.com>
2024-08-29 15:20:57 -0400

    Add events query get request

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -26,15 +26,30 @@ class CalendarsController < ApplicationController
     refresh_and_retry { @calendar_list = service.list_calendar_lists }
   end
 
-  # gets all events from a specific calendar 
+  # gets all events from a specific calendar
+  # def events
+  #   @client.update!(session[:authorization])
+  #   service = Google::Apis::CalendarV3::CalendarService.new
+  #   service.authorization = @client
+  #   @event_list = service.list_events(params[:calendar_id])
+  #   render json: @event_list
+  # rescue Google::Apis::AuthorizationError
+  #   refresh_and_retry { @event_list = service.list_events(params[:calendar_id]) }
+  # end
+
   def events
+    # Do not listen to linter here!
     @client.update!(session[:authorization])
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = @client
-    @event_list = service.list_events(params[:calendar_id])
+    @event_list = service.list_events(params[:calendar_id], time_min: time_min, time_max: time_max)
     render json: @event_list
   rescue Google::Apis::AuthorizationError
-    refresh_and_retry { @event_list = service.list_events(params[:calendar_id]) }
+    refresh_and_retry do
+      @event_list = service.list_events(
+        params[:calendar_id], time_min: time_min, time_max: time_max
+      )
+    end
   end
 
   private
@@ -62,5 +77,18 @@ class CalendarsController < ApplicationController
   rescue StandardError => e
     # Handle error (e.g., log it, show error message)
     redirect_to root_path, alert: "Failed to fetch data: #{e.message}"
+  end
+
+  # returns rfc3339 time
+  def time_min
+    date_string = params[:time_min]
+    time = Time.parse(date_string).utc
+    time.rfc3339
+  end
+
+  def time_max
+    date_string = params[:time_max]
+    time = Time.parse(date_string).utc
+    time.rfc3339
   end
 end
