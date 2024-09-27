96b5af2ef2361ef3d8ea54f6e83c46a853ae4d46
MclPio <113801014+MclPio@users.noreply.github.com>
2024-09-26 23:30:07 -0400

    Add experimental CSRF tokens

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,6 @@ Rails.application.routes.draw do
   # Get time specific events
   # http://localhost:3000/events/primary?time_min=2024-08-01&time_max=2024-09-20
   get "/events/*calendar_id", to: "calendars#events", as: "events", calendar_id: "/[^\/]+/"
+
+  root to: 'application#cookie'
 end
