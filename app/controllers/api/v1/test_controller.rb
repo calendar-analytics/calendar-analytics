fbbff5b48a74ed5f06ed5bdcdcdd5111bea8046b
MclPio <113801014+MclPio@users.noreply.github.com>
2024-10-05 13:17:38 -0400

    Add sample data for api/v1/test

--- app/controllers/api/v1/test_controller.rb
+++ app/controllers/api/v1/test_controller.rb
@@ -1,22 +1,7 @@
 class Api::V1::TestController < ApplicationController
   def index
-    @data = {
-      "items": [
-        {
-          "id": "event_id_1",
-          "summary": "Team Meeting",
-          "description": "Weekly team sync-up meeting",
-          "start": {
-            "dateTime": "2024-08-05T10:00:00-04:00",
-            "timeZone": "America/Toronto"
-          },
-          "end": {
-            "dateTime": "2024-08-05T11:00:00-04:00",
-            "timeZone": "America/Toronto"
-          },
-        }
-      ]
-    }
+    file_path = Rails.root.join('sample.json')
+    @data = File.read(file_path)
 
     render json: @data
   end
